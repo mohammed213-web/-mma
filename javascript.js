937dd863f33954f52eee2e111a75c23b6aac3b04
mohammed213-web <71678623+mohammed213-web@users.noreply.github.com>
2021-05-27 03:07:32 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+        var fighters = ["Conor Megregor", "Khabib Numragomedov", "Jon Jones"] ;
+        var i, len, text;
+        for (i = 0, len = fighters.length, text = ""; i < len; i++) {
+            text += fighters[i] + "<br>"
+        }
+        document.getElementById("demo").innerHTML = text;
+        var x = i;
+        document.getAnimations("demo");
+ 
+  
